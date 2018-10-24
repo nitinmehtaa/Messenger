535f9b096386dbd60d245e185d53203f580061f6
nitinmehtaa <nits.1908@gmail.com>
2018-10-24 14:04:17 +0530

    README updated

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
-# whatsapp_redesign
+# A chat messenger
 
-WhatsApp redesign in Flutter
+Chat Messenger built in Flutter
 
 ## Getting Started
 
 For help getting started with Flutter, view our online
 [documentation](https://flutter.io/).
-
-# WhatsApp-redesign
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   Home(this.listType);
   final String listType;
+  ScrollController _scrollController;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +21,9 @@ class Home extends StatelessWidget {
                   listType,
                   style: new TextStyle(color: const Color(0xFFFFFFFF)),
                 ),
+                background: Image.network(
+                    "https://images.unsplash.com/photo-1540224769541-7e6e20a42330?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b8c2593fda61d509c16c92aea611473d&auto=format&fit=crop&w=500&q=60",
+                    fit: BoxFit.cover),
               ),
             ),
           ];
